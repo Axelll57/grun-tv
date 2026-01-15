8c29d8821f3b6389ab2e1d0729757954649cfd82
Axel ENGINGER <axel.enginger@grun.lu>
2026-01-15 12:57:37 +0100

    Update messages.js

---
+++ assets/messages.js
@@ -0,0 +1,9 @@
+/* LISTE DES MESSAGES DU BANDEAU
+   Tu peux ajouter autant de lignes que tu veux.
+   Le texte change toutes les 20 secondes.
+*/
+
+const bannerMessages = [
+    "BIENVENUE CHEZ GRÜN SIGNALISATION   ---   WËLLKOMM BEI GRÜN SIGNALISATION",
+    "GRÜN SIGNALISATION VOUS SOUHAITE UNE BONNE ANNÉE 2026   ---   GRÜN SIGNALISATION WËNSCHT IECH E GLÉCKLECHT NEIT JOER 2026",
+];
